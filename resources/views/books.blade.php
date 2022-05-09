d1c6ee0ea9d1092f0227c0b1de9acc94315244e4
huytd-it <yukihuy9999@gmail.com>
2022-05-09 23:29:15 +0700

    first commit

---
+++ resources/views/books.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+@section('css')
+@endsection
+@section('content')
+
+@endsection
+@section('js')
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Hyper - Responsive Bootstrap 4 Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <!-- third party css -->
+    <link href="assets/css/vendor/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+    <!-- third party css end -->
+
+    <!-- App css -->
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/app-creative.min.css" rel="stylesheet" type="text/css" id="light-style" />
+    <link href="assets/css/app-creative-dark.min.css" rel="stylesheet" type="text/css" id="dark-style" />
+    @yield('css')
+
+</head>
+
+<body class="loading"
+    data-layout-config='{"leftSideBarTheme":"dark","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
+    <!-- Begin page -->
+    <div class="wrapper">
+        @include('layout.header')
+
+
+        <!-- ============================================================== -->
+        <!-- Start Page Content here -->
+        <!-- ============================================================== -->
+
+        <div class="content-page">
+            <div class="content">
+                @include('layout.menu')
+                <!-- Start Content-->
+                <div class="container-fluid">
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box">
+                                <div class="page-title-right">
+                                    <form class="form-inline">
+                                        <div class="form-group">
+                                            <div class="input-group">
+                                                <input type="text" class="form-control form-control-light"
+                                                    id="dash-daterange">
+                                                <div class="input-group-append">
+                                                    <span class="input-group-text bg-success border-success text-white">
+                                                        <i class="mdi mdi-calendar-range font-13"></i>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <a href="javascript: void(0);" class="btn btn-success ml-2">
+                                            <i class="mdi mdi-autorenew"></i>
+                                        </a>
+                                    </form>
+                                </div>
+                                <h4 class="page-title">Vertical</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+
+                  
+                    <!-- end row -->
+
+
+                </div>
+                <!-- container -->
+
+            </div>
+            <!-- content -->
+            @include('layout.footer')
+
+        </div>
+
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+
+
+    </div>
+    <!-- END wrapper -->
+
+    <!-- Right Sidebar -->
+    <div class="right-bar">
+
+        <div class="rightbar-title">
+            <a href="javascript:void(0);" class="right-bar-toggle float-right">
+                <i class="dripicons-cross noti-icon"></i>
+            </a>
+            <h5 class="m-0">Settings</h5>
+        </div>
+
+        <div class="rightbar-content h-100" data-simplebar>
+
+            <div class="p-3">
+                <div class="alert alert-warning" role="alert">
+                    <strong>Customize </strong> the overall color scheme, layout width, etc.
+                </div>
+
+                <!-- Settings -->
+                <h5 class="mt-3">Color Scheme</h5>
+                <hr class="mt-1" />
+
+                <div class="custom-control custom-switch mb-1">
+                    <input type="radio" class="custom-control-input" name="color-scheme-mode" value="light"
+                        id="light-mode-check" checked />
+                    <label class="custom-control-label" for="light-mode-check">Light Mode</label>
+                </div>
+
+                <div class="custom-control custom-switch mb-1">
+                    <input type="radio" class="custom-control-input" name="color-scheme-mode" value="dark"
+                        id="dark-mode-check" />
+                    <label class="custom-control-label" for="dark-mode-check">Dark Mode</label>
+                </div>
+
+                <!-- Width -->
+                <h5 class="mt-4">Width</h5>
+                <hr class="mt-1" />
+                <div class="custom-control custom-switch mb-1">
+                    <input type="radio" class="custom-control-input" name="width" value="fluid" id="fluid-check"
+                        checked />
+                    <label class="custom-control-label" for="fluid-check">Fluid</label>
+                </div>
+                <div class="custom-control custom-switch mb-1">
+                    <input type="radio" class="custom-control-input" name="width" value="boxed" id="boxed-check" />
+                    <label class="custom-control-label" for="boxed-check">Boxed</label>
+                </div>
+
+
+
+                <button class="btn btn-primary btn-block mt-4" id="resetBtn">Reset to Default</button>
+
+                <a href="https://themes.getbootstrap.com/product/hyper-responsive-admin-dashboard-template/"
+                    class="btn btn-danger btn-block mt-3" target="_blank"><i class="mdi mdi-basket mr-1"></i> Purchase
+                    Now</a>
+            </div> <!-- end padding-->
+
+        </div>
+    </div>
+
+    <div class="rightbar-overlay"></div>
+    <!-- /Right-bar -->
+
+    <!-- bundle -->
+    <script src="assets/js/vendor.min.js"></script>
+    <script src="assets/js/app.min.js"></script>
+
+    <!-- third party js -->
+    <script src="assets/js/vendor/apexcharts.min.js"></script>
+    <script src="assets/js/vendor/jquery-jvectormap-1.2.2.min.js"></script>
+    <script src="assets/js/vendor/jquery-jvectormap-world-mill-en.js"></script>
+    <!-- third party js ends -->
+
+    <!-- demo app -->
+    <script src="assets/js/pages/demo.dashboard.js"></script>
+    <!-- end demo js-->
+    @yield('js')
+</body>
+
+</html>
